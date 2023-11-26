98882d432227653333263a7e8e44c6101f22b78a
Stanislav Vasilchenko <stanilav.vasilchenko@yandex.ru>
2023-11-26 14:43:39 +0300

    some

--- main/admin.py
+++ main/admin.py
@@ -5,10 +5,7 @@ from main.models import Student
 
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
-    list_display = (
-        "first_name",
-        "last_name",
-        "is_activ",
+    list_display = ("first_name","last_name","is_activ",
     )
     list_filter = ("is_activ",)
     search_fields = (
